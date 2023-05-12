8b5c066988baf6f0b5b4a4156b2ef7f1e351867f
warzes <warzes@mail.ru>
2023-05-12 23:37:42 +1000

    - STBImageLoader

---
+++ src/Engine/STBImageLoader.cpp
@@ -0,0 +1,27 @@
+﻿#include "stdafx.h"
+#include "STBImageLoader.h"
+//-----------------------------------------------------------------------------
+STBImageLoader::STBImageLoader(const char* fileName)
+{
+	//stbi_set_flip_vertically_on_load(verticallyFlip ? 1 : 0);
+	const int desiredСhannels = STBI_default;
+	pixelData = stbi_load(fileName, &width, &height, &nrChannels, desiredСhannels);
+	if( !pixelData || nrChannels < STBI_grey || nrChannels > STBI_rgb_alpha || width == 0 || height == 0 )
+	{
+		isValid = false;
+		return;
+	}
+
+	if( nrChannels == STBI_grey ) imageFormat = ImageFormat::R_U8;
+	else if( nrChannels == STBI_grey_alpha ) imageFormat = ImageFormat::RG_U8;
+	else if( nrChannels == STBI_rgb ) imageFormat = ImageFormat::RGB_U8;
+	else if( nrChannels == STBI_rgb_alpha ) imageFormat = ImageFormat::RGBA_U8;
+
+	isValid = true;
+}
+//-----------------------------------------------------------------------------
+STBImageLoader::~STBImageLoader()
+{
+	stbi_image_free((void*)pixelData);
+}
+//-----------------------------------------------------------------------------
---
+++ src/Engine/STBImageLoader.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+enum class ImageFormat : uint8_t
+{
+	None = 0,
+	R_U8,
+	RG_U8,
+	RGB_U8,
+	RGBA_U8,
+};
+
+class STBImageLoader
+{
+public:
+	STBImageLoader(const char* fileName);
+	~STBImageLoader();
+
+	int width = 0;
+	int height = 0;
+	int nrChannels = 0;
+	stbi_uc* pixelData = nullptr;
+	ImageFormat imageFormat = ImageFormat::RGBA_U8;
+
+	bool isValid = false;
+};
