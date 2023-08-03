b82687e0f5d507e441088036d2d809512743bb7e
warzes <warzes@mail.ru>
2023-08-03 23:24:42 +1000

    - delete EASTL

--- src/Engine/stdafx.h
+++ src/Engine/stdafx.h
@@ -24,64 +24,6 @@
 #	include <crtdbg.h>
 #endif
 
-#include <EASTL/algorithm.h>
-#include <EASTL/any.h>
-#include <EASTL/array.h>
-//#include <EASTL/atomic.h>
-#include <EASTL/bitset.h>
-#include <EASTL/bitvector.h>
-#include <EASTL/chrono.h>
-#include <EASTL/deque.h>
-#include <EASTL/finally.h>
-#include <EASTL/fixed_function.h>
-#include <EASTL/fixed_hash_map.h>
-#include <EASTL/fixed_hash_set.h>
-#include <EASTL/fixed_list.h>
-#include <EASTL/fixed_map.h>
-#include <EASTL/fixed_set.h>
-#include <EASTL/fixed_slist.h>
-#include <EASTL/fixed_string.h>
-#include <EASTL/fixed_vector.h>
-#include <EASTL/functional.h>
-#include <EASTL/hash_map.h>
-#include <EASTL/hash_set.h>
-#include <EASTL/intrusive_hash_map.h>
-#include <EASTL/intrusive_hash_set.h>
-#include <EASTL/intrusive_list.h>
-#include <EASTL/intrusive_ptr.h>
-#include <EASTL/iterator.h>
-#include <EASTL/linked_array.h>
-#include <EASTL/linked_ptr.h>
-#include <EASTL/list.h>
-#include <EASTL/map.h>
-#include <EASTL/numeric.h>
-#include <EASTL/numeric_limits.h>
-#include <EASTL/optional.h>
-#include <EASTL/priority_queue.h>
-#include <EASTL/set.h>
-#include <EASTL/shared_array.h>
-#include <EASTL/shared_ptr.h>
-#include <EASTL/slist.h>
-#include <EASTL/sort.h>
-#include <EASTL/span.h>
-#include <EASTL/stack.h>
-#include <EASTL/string.h>
-#include <EASTL/tuple.h>
-#include <EASTL/type_traits.h>
-#include <EASTL/unique_ptr.h>
-#include <EASTL/unordered_map.h>
-#include <EASTL/unordered_set.h>
-#include <EASTL/utility.h>
-#include <EASTL/variant.h>
-#include <EASTL/vector.h>
-#include <EASTL/vector_map.h>
-#include <EASTL/vector_multimap.h>
-#include <EASTL/vector_multiset.h>
-#include <EASTL/vector_set.h>
-#include <EASTL/weak_ptr.h>
-
-namespace ea = eastl;
-
 #include <memory>
 #include <algorithm>
 #include <filesystem>
